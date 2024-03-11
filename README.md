03cffa8e8db48237071251831294dc6ede4fbd51
Eva Evgenidis <134018539+eva-e@users.noreply.github.com>
2024-03-11 22:57:12 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+
+This template was made via [this tutorial](https://deno.com/blog/build-a-blog-with-fresh).
+
---
+++ utils/posts.ts
@@ -0,0 +1,40 @@
+import { extract } from "$std/encoding/front_matter.ts";
+import { join } from "$std/path/posix.ts";
+
+const DIRECTORY = "./posts";
+
+export interface Post {
+  slug: string;
+  title: string;
+  publishedAt: Date;
+  snippet: string;
+  content: string;
+  disableHtmlSanitization: boolean;
+}
+
+// Get posts.
+export async function getPosts(): Promise<Post[]> {
+  const files = Deno.readDir(DIRECTORY);
+  const promises = [];
+  for await (const file of files) {
+    const slug = file.name.replace(".md", "");
+    promises.push(getPost(slug));
+  }
+  const posts = await Promise.all(promises) as Post[];
+  posts.sort((a, b) => b.publishedAt.getTime() - a.publishedAt.getTime());
+  return posts;
+}
+
+// Get post.
+export async function getPost(slug: string): Promise<Post | null> {
+  const text = await Deno.readTextFile(join(DIRECTORY, `${slug}.md`));
+  const { attrs, body } = extract(text);
+  return {
+    slug,
+    title: attrs.title,
+    publishedAt: new Date(attrs.published_at),
+    content: body,
+    snippet: attrs.snippet,
+    disableHtmlSanitization: attrs.disable_html_sanitization,
+  };
+}
