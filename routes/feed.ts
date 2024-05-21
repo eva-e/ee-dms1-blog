ee029780adc7e7c47d50d722b84b97c8647b8844
Eva Evgenidis <134018539+eva-e@users.noreply.github.com>
2024-05-21 23:42:22 +1000

    title change test

--- routes/feed.ts
+++ routes/feed.ts
@@ -9,7 +9,7 @@ export const handler: Handlers<Post[]> = {
     const origin = url.origin;
     const copyright = `Copyright ${new Date().getFullYear()} ${origin}`;
     const feed = new Feed({
-      title: "Blog",
+      title: "Eva's Blog",
       description: "This is a Fresh Blog",
       id: `${origin}/blog`,
       link: `${origin}/blog`,
