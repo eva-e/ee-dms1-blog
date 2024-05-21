78e8d8343cba3598da2ad7249ce7423ff5322193
Eva Evgenidis <134018539+eva-e@users.noreply.github.com>
2024-05-21 23:46:10 +1000

    title change test 2

--- routes/index.tsx
+++ routes/index.tsx
@@ -12,7 +12,7 @@ export default function BlogIndexPage(props: PageProps<Post[]>) {
   const posts = props.data;
   return (
     <main class="max-w-screen-md px-4 pt-16 mx-auto">
-      <h1 class="text-5xl font-bold">Blog</h1>
+      <h1 class="text-5xl font-bold">Eva's Blog</h1>
       <div class="mt-8">
         {posts.map((post) => <PostCard post={post} />)}
       </div>
